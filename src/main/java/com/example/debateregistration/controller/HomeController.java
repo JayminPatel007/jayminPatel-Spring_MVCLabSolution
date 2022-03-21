44ecea7257ba029b4c3a9cce4d97d4d88ef9dae6
jayminpatel007 <jaymin.future365@gmail.com>
2022-03-22 01:12:54 +0530

    delete and edit

--- src/main/java/com/example/debateregistration/controller/HomeController.java
+++ src/main/java/com/example/debateregistration/controller/HomeController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -45,5 +42,20 @@ public class HomeController {
         session.setAttribute("msg", "Your Registration added successfully");
 
         return "redirect:/add";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Student student, HttpSession session) {
+        this.studentRepository.save(student);
+        session.setAttribute("msg", "Your Registration updated successfully");
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable(value = "id") long id, HttpSession session) {
+        this.studentRepository.deleteById(id);
+        session.setAttribute("msg", "Registration deleted successfully");
+        return "redirect:/";
     }
 }
