bd8338db5b94f37dea21418f3efc213405644fe4
jayminpatel007 <jaymin.future365@gmail.com>
2022-03-22 00:48:49 +0530

    list all students

--- src/main/java/com/example/debateregistration/controller/HomeController.java
+++ src/main/java/com/example/debateregistration/controller/HomeController.java
@@ -1,12 +1,28 @@
 package com.example.debateregistration.controller;
 
+import com.example.debateregistration.entity.Student;
+import com.example.debateregistration.repository.StudentRepository;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private StudentRepository studentRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model m) {
+        List<Student> list = this.studentRepository.findAll();
+        m.addAttribute("all_student_regs", list);
         return "index";
     }
 
@@ -15,8 +31,19 @@ public class HomeController {
         return "add";
     }
 
-    @GetMapping("/edit")
-    public String edit() {
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable(value = "id") long id, Model m) {
+        Optional<Student> student = this.studentRepository.findById(id);
+        Student st = student.get();
+        m.addAttribute("student", st);
         return "edit";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Student student, HttpSession session) {
+        this.studentRepository.save(student);
+        session.setAttribute("msg", "Your Registration added successfully");
+
+        return "redirect:/add";
     }
 }
